fb4b23eed9fe2eaae5c79ac911c38108be36adfe
ema sestakova <160266789+4ki-47@users.noreply.github.com>
2026-06-22 15:23:10 -0400

    Create model.py

---
+++ ema-sestakova/raschka-LLM/model.py
@@ -0,0 +1,200 @@
+# LLM Architecture
+import torch
+import torch.nn as nn
+import tiktoken
+import matplotlib.pyplot as plt
+from attention_mechanism import MultiHeadAttention
+
+# ---------------------------------------------------------------------------
+# GPT Backbone
+
+# Dictionary of the GPT-2 model configuration
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
+
+# ---------------------------------------------------------------------------
+# Layer Normalization
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+        
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+# ---------------------------------------------------------------------------
+# GELU activation
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+# ReLU is a piecewise linear function that outputs the input 
+# directly if it is positive; otherwise, it outputs zero.
+# ------------------------------
+# GELU is a smooth, nonlinear function that approximates ReLU but 
+# with a non-zero gradient for almost all negative values
+
+# ---------------------------------------------------------------------------
+# Feed forward network
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        # input tensor --> linear layer --> GELU activation 
+        #       --> linear layer --> output tensor
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+# ---------------------------------------------------------------------------
+# Transformer block
+
+# input --> *(shortcut connection) --> layer norm 1 --> masked multi-head attention
+#   --> dropout * --> *(shortcut connection) --> layer norm 2
+#       --> feed forward (linear layer --> GELU --> linear layer) --> dropout * --> output
+# outputs have the same form and dimensions as the inputs
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        # add the original input back
+        x = x + shortcut
+
+        # shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        # add the original input back
+        x = x + shortcut
+        return x
+
+# ---------------------------------------------------------------------------
+# The GPT model architecture implementation
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+  
+# ---------------------------------------------------------------------------
+# Generating Text
+
+# INPUT: "Hello, I am", single iteration:
+# 1. Encodes text input into four token IDs
+# 2. The GPT model returns a matrix consisting of four vectors (rows), 
+#       where each vector
+#       has 50257 dimensions (columns).
+# 3. Extracts the last vector, which corresponds to the next token that the
+#       GPT model is supposed to generate
+# 4. Converts logits into probability distribution using the softmax function
+# 5. Identifies the index position of the largest value, which also 
+#       represents the token ID
+# 6. Appends token to the previous inputs for the next round
+
+# A function for the GPT model to generate text
+# a generative loop for a language model using PyTorch
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        # Crops current context if it exceeds the supported context size
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        # Focuses only on the last time step, so that (batch, n_token, vocab_size)
+        # becomes (batch, vocab_size)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1) # shape (batch, vocab_size)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True) # shape (batch, 1)
+        # Appends sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1) # shape (batch, n_tokens+1)
+    return idx
+
+# ---------------------------------------------------------------------------
+# Example
+
+tokenizer = tiktoken.get_encoding("gpt2")
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+# eval() disables dropout since we are not training the model
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output:", out)
+print("Output length:", len(out[0]))
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
